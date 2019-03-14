ecd517862044d0d0bafbe626469be4fc99b89d25
kittysee <47625293+kittysee@users.noreply.github.com>
2019-03-14 15:40:10 +0800

    Update mysr.c

--- mysr.c
+++ mysr.c
@@ -17,14 +17,15 @@ void mysr_init_sender(struct mysr_sender* mysr_sender, char* ip, int port, int N
 }
 
 int mysr_send(struct mysr_sender* mysr_sender, unsigned char* buf, int len){
-        //Data pack
+        //DataPack
         struct MYSR_Packet data;
         data.protocol[0] = 's';
         data.protocol[1] = 'r';
         data.type = 0xA0;
         data.seqNum = 0;
         data.length = sizeof(data) + len;
-
+        
+        //EndPack
         struct MYSR_Packet end;
         end.protocol[0] = 's';
         end.protocol[1] = 'r';
@@ -51,9 +52,9 @@ int mysr_send(struct mysr_sender* mysr_sender, unsigned char* buf, int len){
         memset(sent, 0, sizeof(int));
         int size = sizeof(mysr_sender->server_addr);
         int i;
-        //Send base + window_size - 1 packets
+        
+        //Send (base + window_size - 1) packets
         for(i = 0; i <= window_end; i++){
-                //send file
                 data.seqNum = i;
                 sendto(mysr_sender->sd, &data, sizeof(data), 0, &(mysr_sender->server_addr), sizeof(mysr_sender->server_
 addr));
@@ -63,7 +64,7 @@ server_addr), sizeof(mysr_sender->server_addr));
         }
 
         while(acked_packet < packetNum){
-                //Set timer for the base
+                //Set timer
                 fd_set fds;
                 FD_ZERO(&fds);
                 FD_SET(mysr_sender->sd, &fds);
@@ -75,17 +76,18 @@ server_addr), sizeof(mysr_sender->server_addr));
                 if((ready = select((mysr_sender->sd) + 1, &fds, NULL, NULL, &timeout)) < 0){
                         printf("%s",strerror(errno));
                 }
+                
                 if(ready>0){
                         //recv ACK
                         if(recvfrom(mysr_sender->sd, (struct MYSR_Packet *)&ACK, sizeof(ACK), 0, &(mysr_sender->server_a
 ddr), &size) < 0){
-
+                                printf("Recv Error:%s\n", strerror(errno));
                         }else{
                                 ACKed[ACK.seqNum] = 1;
                                 acked_packet++;
                         }
 
-                        //Move the window
+                        //Slide the window
                         if (base == ACK.seqNum){
                                 if(window_end > packetNum){
                                         window_end = packetNum - 1;
@@ -113,7 +115,8 @@ ddr), &size) < 0){
                                         }
                                 }
                         }
-                        //send file
+                        
+                        //send non-sent files in the window
                         for (i = base; i <= window_end; i++){
                                 data.seqNum = i;
                                 if(sent[i] == 0 && window_end < packetNum){
@@ -137,6 +140,7 @@ r_sender->server_addr), sizeof(mysr_sender->server_addr));
                 }
         }
 
+        //Send EndPack to end this transition
         struct MYSR_Packet recv_end;
         int isend = 0;
         sendto(mysr_sender->sd, (struct MYSR_Packet *)&end, sizeof(end), 0, &(mysr_sender->server_addr), sizeof(mysr_sen
@@ -159,7 +163,7 @@ erver_addr), &size);
                         return return_size;
                 }else{
                         if (isend > 3){
-                                return 0;
+                                return return_size;
                         }
                         isend++;
                         sendto(mysr_sender->sd, (struct MYSR_Packet *)&end, sizeof(end), 0, &(mysr_sender->server_addr),
@@ -251,6 +255,7 @@ int mysr_recv(struct mysr_receiver* mysr_receiver, unsigned char* buf, int len){
                 if((tmp = recvfrom(mysr_receiver->sd, (struct MYSR_Packet*)&data, sizeof(struct MYSR_Packet), 0, (struct
  sockaddr *)&mysr_receiver->address, &address_size)) >= 0){
                         if (data.type == 0xA0){
+                                //recv DataPack
                                 if ((tmp = recvfrom(mysr_receiver->sd, (unsigned char *)&buf[data.seqNum], sizeof(buf[da
 ta.seqNum]), 0 ,(struct sockaddr *)&mysr_receiver->address, &address_size)) < 0 ){
                                         printf("%s\n", strerror(errno));
@@ -262,10 +267,12 @@ ta.seqNum]), 0 ,(struct sockaddr *)&mysr_receiver->address, &address_size)) < 0 
 ckaddr *)&mysr_receiver->address, sizeof(mysr_receiver->address));
                                 }
                         }else if ((data.type == 0xA2) && (data.seqNum == 0)){
+                                //recv EndPack for the transition
                                 sendto(mysr_receiver->sd, (struct MYSR_Packet *)&end, sizeof(end), 0 ,(struct sockaddr *
 )&mysr_receiver->address, address_size);
                                 return return_size;
                         }else if ((data.type == 0xA2) && (data.seqNum == 1)){
+                                //recv EndPack for the Client
                                 sendto(mysr_receiver->sd, (struct MYSR_Packet *)&ACK, sizeof(ACK), 0 ,(struct sockaddr *
 )&mysr_receiver->address, address_size);
 
